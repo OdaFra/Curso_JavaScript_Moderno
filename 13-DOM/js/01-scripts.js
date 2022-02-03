6a0e414e3d48e870875973132f451dced6485fc2
Oscar <osramirezf@gmail.com>
2022-02-03 20:25:13 -0300

    Se ha desarrollado DOM

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,15 @@
+let elemento;
+
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].action;
+
+console.log(elemento);
