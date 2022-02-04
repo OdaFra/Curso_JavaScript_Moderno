442607c27599e4d372fba5b50423a869c6d1a58a
OdaFra <odafra@MacBook-Pro-de-OdaFra.local>
2022-02-03 21:09:53 -0300

    Ejercicio DOM - Acceder elementos de HTML con document

--- 13-DOM/js/01-scripts.js
+++ 13-DOM/js/01-scripts.js
@@ -11,5 +11,10 @@ elemento = document.forms[0].id;
 elemento = document.forms[0].method;
 elemento = document.forms[0].classList;
 elemento = document.forms[0].action;
+elemento = document.links[4];
+elemento = document.links[4].classList;
+elemento = document.links[4].className;
+elemento= document.images;
+elemento= document.scripts;
 
 console.log(elemento);
