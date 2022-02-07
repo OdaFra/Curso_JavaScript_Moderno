7b363b278478ed33cdf7afe5c225c0560cd6af41
Oscar <osramirezf@gmail.com>
2022-02-07 15:12:16 -0300

    Se ha desarrolado sección 13 Dom, JS 1, 2 y 3

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,9 @@
+//Por id
+
+const formulario = document.getElementById("formulario"); // va retornar máximo un resultado
+
+console.log(formulario);
+
+const noExiste = document.getElementById("noexiste");
+
+console.log(noExiste);
