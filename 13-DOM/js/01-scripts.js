986ca2db882ca8ad40ac2dc3c933243eec3fb3d7
Oscar <osramirezf@gmail.com>
2022-02-07 14:55:20 -0300

    Sección de DOM

--- 13-DOM/js/01-scripts.js
+++ 13-DOM/js/01-scripts.js
@@ -11,10 +11,11 @@ elemento = document.forms[0].id;
 elemento = document.forms[0].method;
 elemento = document.forms[0].classList;
 elemento = document.forms[0].action;
+elemento = document.links;
 elemento = document.links[4];
 elemento = document.links[4].classList;
 elemento = document.links[4].className;
-elemento= document.images;
-elemento= document.scripts;
+elemento = document.images;
+elemento = document.scripts;
 
 console.log(elemento);
