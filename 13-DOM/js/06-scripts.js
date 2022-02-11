e2c5e8803503aad4559a171b7694eab755b4cc31
Oscar <osramirezf@gmail.com>
2022-02-11 17:31:35 -0300

    Se ha realizado ejercicios 5 y 6 de la sección 13 Manejo de DOM

---
+++ 13-DOM/js/06-scripts.js
@@ -0,0 +1,12 @@
+const encabezado = document.querySelector(".contenido-hero h1").textContent;
+
+console.log(encabezado);
+// console.log(encabezado.innerText); // Si en el CSS - Visibility:hidder, no lo va a encontrar
+// console.log(encabezado.textContent); // Si lo va a encontrar
+// console.log(encabezado.innerHTML); // Se trae el HTML
+
+// const newheading = "Nuevo Heading";
+// document.querySelector(".contenido-hero h1").textContent = newheading;
+
+// const img = document.querySelector(".card img");
+// img.src = "img/hacer2.jpg";
